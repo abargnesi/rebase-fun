0d4d98227b53ca870d2f470f2bdf58c998adfeb6
jhourani <jordanhourani@gmail.com>
2012-08-10 10:06:44 -0400

    Now includes a bomb

--- project-a/src/main/java/com/acme/projectA/Main.java
+++ project-a/src/main/java/com/acme/projectA/Main.java
@@ -2,12 +2,16 @@ package com.acme.projectA;
 
 public class Main {
 
+    private static final int COUNT = 10;
+    private BigBoom bomb;
+
     public static void main(final String[] args) {
 
 	for (int i = 0; i < 10; i++)
         	System.out.println("Hello World");
 
-	System.out.println("End of test.");
+	bomb = new BigBoom(COUNT);
+	bomb.makeAboom();
 	
     }
 }
