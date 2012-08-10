acbcdab065a60c3797308f6df2a6f3aea2a5e841
Anthony Bargnesi <abargnesi@gmail.com>
2012-08-10 09:03:00 -0400

    created projectA
    added main class
    added maven pom
    added manifest
    
    added property
    
    added Types class

---
+++ project-a/src/main/java/com/acme/projectA/Main.java
@@ -0,0 +1,9 @@
+package com.acme.projectA;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
---
+++ project-a/src/main/java/com/acme/projectA/Types.java
@@ -0,0 +1,8 @@
+package com.acme.projectA;
+
+public enum Types {
+    TYPE_1,
+    TYPE_2,
+    TYPE_3;
+}
+
